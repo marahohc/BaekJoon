d402b0554b76648abef6080486bdd1a6eb9123f2
조하람 <96278814+marahohc@users.noreply.github.com>
2026-03-04 12:02:36 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 48 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+class HelloWorld {
+  static void Main() {
+      string input = Console.ReadLine();
+      Console.WriteLine(1);
+      Console.Write(0);
+  }
+}
